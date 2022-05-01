8c59d23153bce58bf0179db395cc9a04c42f7560
Celia Kelly <celiackelly@gmail.com>
2022-04-30 21:17:29 -0400

    Add basic 2-player game

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
+const divs = document.querySelectorAll('div')
+const arr = divs.forEach(div => {
+  div.addEventListener('click', markSquare)
+})
 
+document.querySelector('button').addEventListener('click', clearBoard)
+
+let turn = 2
+let xChoices = []
+let oChoices = []
+
+function markSquare(event) {
+  if(event.target.textContent === 'X' || event.target.textContent === 'O'){
+    alert("Error - square is already filled")
+  }else{
+    if(turn % 2 === 0 ){
+      event.target.textContent = 'X'
+      turn++
+      event.target.classList.remove('empty') 
+      xChoices.push(Number(event.target.id))
+    }else{
+      event.target.textContent = 'O'
+      turn++
+      event.target.classList.remove('empty') 
+      oChoices.push(Number(event.target.id))
+    }
+  }if(turn === 12){
+    alert("Game is over - you tied")
+  }  
+  checkWin()
+}
+
+
+function checkWin (){
+  xChoices.sort((a,b) => a-b)
+  oChoices.sort((x,y) => x-y)
+  for (let i = 0; i < winConditions.win.length; i++) {
+    if(winConditions.win[i].every(el => xChoices.includes(el))){
+      alert('Player X wins! Please press Reset')
+    }else if(winConditions.win[i].every(el => oChoices.includes(el))){
+      alert('Player O wins! Please press Reset')
+    } 
+    }
+  }
+
+const winConditions = {
+  win: [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
+}
+
+function clearBoard(){
+  turn = 2
+  divs.forEach(div => div.textContent = '')
+  xChoices = []
+  oChoices = []
+}
